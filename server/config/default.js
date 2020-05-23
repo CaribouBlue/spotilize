1d85493aefbe6608215206ae92534f59c6748f9a
Alec Draymore <Draymore26@gmail.com>
2020-05-23 19:52:12 -0400

    added morgan middleware
    for logging requests + metrics

--- server/config/default.js
+++ server/config/default.js
@@ -1,5 +1,6 @@
 module.exports = {
   server: {
     port: process.env.PORT || 1337,
+    morganFormat: 'tiny',
   }
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -7,4 +7,13 @@ import morgan from 'morgan'
 const app = express()
 const port: string | number = config.get('server.port')
 
+// MIDDLEWARE
+const morganFormat: string = config.get('server.morganFormat')
+app.use(morgan(morganFormat))
+
+//ROUTES
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
 export {app, port}
