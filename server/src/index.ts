9079d400c3bed2130fa042cb6de209559c57813c
Alec Draymore <Draymore26@gmail.com>
2020-05-23 13:07:35 -0400

    inital set up for config module
    set up config dir
    added message to config re: config version being used
    added default and development config.js files

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,10 @@
 'use strict';
 
-const message: string = 'hello world'
-console.log(message)
+const path = require('path')
+
+// set up config
+process.env['NODE_CONFIG_DIR'] = path.resolve(__dirname, '../config/')
+const deployment: string = process.env['NODE_ENV'] || 'default'
+const instance: string | null = process.env['NODE_APP_INSTANCE'] || null;
+const configFile = deployment + (instance ? '-' + instance : '')
+console.info(`Using config ${configFile}`)
