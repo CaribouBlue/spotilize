28d80addb483f8e1af700ff6a6248c45b13b0af0
Alec Draymore <Draymore26@gmail.com>
2020-05-27 22:23:35 -0400

    added type for spotify artist

--- server/src/services/spotify/types.ts
+++ server/src/services/spotify/types.ts
@@ -137,3 +137,21 @@ export interface ArtistAbbrv {
   type: string,
   uri: string
 }
+
+export interface Artist {
+  external_urls: {
+    spotify: string
+  },
+  followers: {
+    href: string,
+    total: number
+  },
+  genres: [string],
+  href: string,
+  id: string,
+  images: Image[],
+  name: string,
+  popularity: number,
+  type: string,
+  uri: string
+}
